6a6b6aa35229ad66af0478ec695857a9c642d860
RobisonCSSE <alexander.robisoncs@gmail.com>
2023-10-23 08:31:28 -0400

    Var and time sleep Stable
    d

--- ProjectOrangatan.py
+++ ProjectOrangatan.py
@@ -2,8 +2,11 @@
 Our Welcome Screen will start our program letting
 drivers know that the Infotech Center 2023 is loading
 """
+#Libraries Here
+import time
 
+timeToSleep =2
 
 print("\nWelcome - InfoTech Center 2023")
-
+time.sleep(timeToSleep)
 print("InfoTech Center 2023 is loading...")
