e9965b26a513703d6069fa4fedc86575d99e4f15
RobisonCSSE <alexander.robisoncs@gmail.com>
2023-10-30 08:40:31 -0400

    add List of Gastations nearby

--- SandboxPlayground.py
+++ SandboxPlayground.py
@@ -11,8 +11,15 @@ def gasLevelGauge():
     currentGasLevel = random.choice(gasLevelList)
     return currentGasLevel
 
+# Function with a list of GasStations
+def listOfGasStations():
+    gasStations = ["Shells","Speedway", "Exxon", "Seven Eleven", "Meijer", "Costco" ,"Marathon", "BP", "cirlce K", "Wesco"]
+    gasStationsNearby = random.choice(gasStations)
+    return gasStationsNearby
+
+
+
 #function will call the gasLevelGauge to determine gas level  and then find a close gas station if low
 def gasLevelAlert():
     milesToGasStationsLow = round(random.uniform(1, 25), 1)
     milesToGasStationsQuarter = round(random.uniform(25.1, 50), 1)
-    
