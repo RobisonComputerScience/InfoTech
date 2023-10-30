fb5fca27c52889690bc593e12d2f70e41dff923d
RobisonCSSE <alexander.robisoncs@gmail.com>
2023-10-30 08:17:41 -0400

    Added gasLevelAlert Function - milesToGasStation variable
    variable random/round

--- SandboxPlayground.py
+++ SandboxPlayground.py
@@ -5,10 +5,15 @@ print("Gasoline Branch\n\n")
 import random
 from time import sleep
 
+# function that list gas stations and randomly chooses one, and then returns its value
 def gasLevelGauge():
     gasLevelList = ["Empty Tank","Low Tank","Quarter Tank","Half Tank","Three Quarter Tank","Full Tank"]
     currentGasLevel = random.choice(gasLevelList)
     return currentGasLevel
 
+#function will call the gasLevelGauge to determine gas level  and then find a close gas station if low
+def gasLevelAlert():
+    milesToGasStationsLow = round(random.uniform(1,25),1)
+    print(milesToGasStationsLow)
 
-
+gasLevelAlert()
