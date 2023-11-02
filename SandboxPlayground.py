e2121c0930c289b322960e089662891a54f69821
RobisonCSSE <alexander.robisoncs@gmail.com>
2023-11-02 08:41:41 -0400

    new things

--- SandboxPlayground.py
+++ SandboxPlayground.py
@@ -1,9 +1,41 @@
-print("**************************************************************")
-print("Gasoline Branch\n\n")
+"""
+Our Welcome Screen will start our program letting
+drivers know that the InfoTech Center 2023 is loading
+"""
 
-# Import Libraries here
+#Libraries Here
+import time
+import sys
 import random
 from time import sleep
+timeToSleep =2
+
+print("\nWelcome - InfoTech Center 2023")
+time.sleep(timeToSleep)
+
+#add code to have the ellipsis add a dot every .5 seconds
+x = 0
+ellipsis = 0
+
+while x != 20:
+    x += 1
+    message = ("InfoTech Center 2023 is loading" + "." * ellipsis)
+    ellipsis = ellipsis + 1
+    sys.stdout.write("\r" + message) #\r prints a carriage return first, so, message is printed on top of the previous line
+    time.sleep(.5)
+    if ellipsis == 4:
+        ellipsis = 0
+    if x == 20:
+        print("\nAccess granted ")
+
+
+
+print("\n\n**************************************************************")
+print("Checking current gas levels\n\n")
+sleep(1)
+
+
+
 # function that list gas stations and randomly chooses one, and then returns its value
 def gasLevelGauge():
     gasLevelList = ["Empty Tank","Low Tank","Quarter Tank","Half Tank","Three Quarter Tank","Full Tank"]
@@ -43,4 +75,6 @@ def gasLevelAlert():
 
 
 gasLevelAlert()
+
+
 
