2aa289128f10d62cb503448bad67c287d19fea71
RobisonCSSE <alexander.robisoncs@gmail.com>
2023-11-02 08:10:21 -0400

    add gaslevelalert function w/ if elif and else statements

--- SandboxPlayground.py
+++ SandboxPlayground.py
@@ -4,7 +4,6 @@ print("Gasoline Branch\n\n")
 # Import Libraries here
 import random
 from time import sleep
-
 # function that list gas stations and randomly chooses one, and then returns its value
 def gasLevelGauge():
     gasLevelList = ["Empty Tank","Low Tank","Quarter Tank","Half Tank","Three Quarter Tank","Full Tank"]
@@ -21,15 +20,26 @@ def listOfGasStations():
 def gasLevelAlert():
     milesToGasStationsLow = round(random.uniform(1, 25), 1)
     milesToGasStationsQuarter = round(random.uniform(25.1, 50), 1)
-    gasLevelGauge()
-    if gasLevelGauge == "Empty Tank":
-        print("***WARNING - YOU ARE ON EMPTY***")
-        sleep(1.5)
+    gasLevelIndicator = gasLevelGauge()
+    if gasLevelIndicator == "Empty Tank":
+        print("Womp Womp shoulda gotten gas")
+        sleep(1.25)
         print("Calling Triple AAA")
-    elif gasLevelGauge == "Low Tank":
+    elif gasLevelIndicator == "Low Tank":
         print("your gas tank is extremely low, checking google maps for the closest gas station")
         sleep(1.5)
-        print("The closest gas statoin is", listOfGasStations(), "Which is",milesToGasStationsLow, "miles away.")
+        print("The closest gas station is", listOfGasStations(), "Which is",milesToGasStationsLow, "miles away.")
+    elif gasLevelIndicator == "Quarter Tank":
+        print("your gas tank is about a quarter of a tank, checking google maps for the closest gas station")
+        sleep(1.5)
+        print("The closest gas station is", listOfGasStations(), "Which is", milesToGasStationsQuarter, "miles away.")
+    elif gasLevelIndicator == "Half Tank":
+        print("your gas tank is about half of a tank.")
+    elif gasLevelIndicator == "Three Quarter Tank":
+        print("You'll make it")
+    else:
+        print("WOOO")
+
 
 
 gasLevelAlert()
