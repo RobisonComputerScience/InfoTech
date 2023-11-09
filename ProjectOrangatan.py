6210ec4f9879c57310ea4d57588e656ba6a90428
RobisonCSSE <alexander.robisoncs@gmail.com>
2023-11-09 08:25:18 -0500

    Moved Libraries and Updated Code

--- ProjectOrangatan.py
+++ ProjectOrangatan.py
@@ -6,6 +6,8 @@ drivers know that the Infotech Center 2023 is loading
 #Libraries Here
 import time
 import sys
+import random
+from time import sleep
 
 
 timeToSleep = 2
@@ -19,21 +21,20 @@ ellipsis = 0
 
 while x != 20:
     x += 1
-    message = ("infoTech Center 2023 is loading" + "." * ellipsis)
+    message = ("InfoTech Center 2023 is loading" + "." * ellipsis)
     ellipsis = ellipsis + 1
     sys.stdout.write("\r" + message) # \r prints a carriage return first so message stays on same line
     time.sleep(.5)
     if ellipsis == 4:
         ellipsis = 0
     if x ==20:
-        print("\n\n Operating system loaded")
+        print("\n\n Operating system loaded\n")
 
-print("**************************************************************")
-print("Gasoline Branch\n\n")
+print("**************************************************************\n")
+print("Checking current gasoline levels.\n")
 
-# Import Libraries here
-import random
-from time import sleep
+sleep(1)
+
 # function that list gas stations and randomly chooses one, and then returns its value
 def gasLevelGauge():
     gasLevelList = ["Empty Tank","Low Tank","Quarter Tank","Half Tank","Three Quarter Tank","Full Tank"]
@@ -52,15 +53,15 @@ def gasLevelAlert():
     milesToGasStationsQuarter = round(random.uniform(25.1, 50), 1)
     gasLevelIndicator = gasLevelGauge()
     if gasLevelIndicator == "Empty Tank":
-        print("Youre on empty Womp Womp shoulda gotten gas")
+        print("You're on empty Womp Womp shoulda gotten gas")
         sleep(.5)
         print("Calling Triple AAA")
     elif gasLevelIndicator == "Low Tank":
-        print("your gas tank is extremely low, checking google maps for the closest gas station")
+        print("Your gas tank is extremely low, checking google maps for the closest gas station")
         sleep(.5)
         print("The closest gas station is", listOfGasStations(), "Which is",milesToGasStationsLow, "miles away.")
     elif gasLevelIndicator == "Quarter Tank":
-        print("your gas tank is about a quarter of a tank, checking google maps for the closest gas station")
+        print("Your gas tank is about a quarter of a tank, checking google maps for the closest gas station")
         sleep(.5)
         print("The closest gas station is", listOfGasStations(), "Which is", milesToGasStationsQuarter, "miles away.")
     elif gasLevelIndicator == "Half Tank":
