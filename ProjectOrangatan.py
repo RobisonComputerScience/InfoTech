0a4e018309326f77fbc36d735a9c7c6a69fb6fe2
RobisonCSSE <alexander.robisoncs@gmail.com>
2023-11-09 08:08:13 -0500

    Welcome Branch - Run & stable

--- ProjectOrangatan.py
+++ ProjectOrangatan.py
@@ -4,9 +4,26 @@ drivers know that the Infotech Center 2023 is loading
 """
 #Libraries Here
 import time
+import sys
 
-timeToSleep =2
+
+timeToSleep = 2
 
 print("\nWelcome - InfoTech Center 2023")
 time.sleep(timeToSleep)
-print("InfoTech Center 2023 is loading...")
+
+#add code to have the ellipsis add a dot every .5 seconds
+x = 0
+ellipsis = 0
+
+while x != 20:
+    x += 1
+    message = ("infoTech Center 2023 is loading" + "." * ellipsis)
+    ellipsis = ellipsis + 1
+    sys.stdout.write("\r" + message) # \r prints a carriage return first so message stays on same line
+    time.sleep(.5)
+    if ellipsis == 4:
+        ellipsis = 0
+    if x ==20:
+        print("\n\n Operating system loaded")
+
